418675996a1ae95da9d72cfb14fa539c4e02d1c6
Sumit Kadu <Sumit.Kadu@wkfs-frc.local>
2018-07-20 17:59:38 +0530

    Iris Clustering Sample Added

---
+++ IrisClustering/IrisClustering/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.ML.Trainers;
+using Microsoft.ML.Transforms;
+using System.Threading.Tasks;
+
+namespace IrisClustering
+{
+    class Program
+    {
+        static readonly string _dataPath = Path.Combine(Environment.CurrentDirectory, "Data", "iris.data");
+        static readonly string _modelPath = Path.Combine(Environment.CurrentDirectory, "Data", "IrisClusteringModel.zip");
+
+        private static async Task Main(string[] args)
+        {
+            PredictionModel<IrisData, ClusterPrediction> model = Train();
+            await model.WriteAsync(_modelPath);
+            var prediction = model.Predict(TestIrisData.Setosa);
+            Console.WriteLine($"Cluster: {prediction.PredictedClusterId}");
+            Console.WriteLine($"Distances: {string.Join(" ", prediction.Distances)}");
+            Console.ReadKey();
+        }
+
+        private static PredictionModel<IrisData, ClusterPrediction> Train()
+        {
+            var pipeline = new LearningPipeline();
+            pipeline.Add(new TextLoader(_dataPath).CreateFrom<IrisData>(separator: ','));
+            pipeline.Add(new ColumnConcatenator(
+                "Features",
+                "SepalLength",
+                "SepalWidth",
+                "PetalLength",
+                "PetalWidth"));
+            pipeline.Add(new KMeansPlusPlusClusterer() { K = 3 });
+            var model = pipeline.Train<IrisData, ClusterPrediction>();
+            return model;
+        }
+    }
+}
---
+++ IrisClustering/IrisClustering/TestIrisData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IrisClustering
+{
+    static class TestIrisData
+    {
+        internal static readonly IrisData Setosa = new IrisData
+        {
+            SepalLength = 5.1f,
+            SepalWidth = 3.5f,
+            PetalLength = 1.4f,
+            PetalWidth = 0.2f
+        };
+    }
+}
