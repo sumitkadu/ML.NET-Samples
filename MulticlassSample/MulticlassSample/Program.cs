18e6bb0c630704cfb161509cd6f2456c4ca8e052
Sumit Kadu <sumit.kadu@mindbodyonline.com>
2019-12-10 13:40:22 +0530

    Added Multiclass & Tensorflow sample

---
+++ MulticlassSample/MulticlassSample/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.ML;
+
+namespace MulticlassSample
+{
+    class Program
+    {
+        private static string _appPath => Path.GetDirectoryName(Environment.GetCommandLineArgs()[0]);
+        private static string _trainDataPath => Path.Combine(_appPath, "..", "..", "..", "Data", "train_data.txt");
+        private static string _testDataPath => Path.Combine(_appPath, "..", "..", "..", "Data", "test_data.txt");
+        private static string _modelPath => Path.Combine(_appPath, "..", "..", "..", "Models", "model.zip");
+
+        private static MLContext _mlContext;
+        private static PredictionEngine<SentimentData, SentimentPrediction> _predEngine;
+        private static ITransformer _trainedModel;
+        static IDataView _trainingDataView;
+
+        static void Main(string[] args)
+        {
+            _mlContext = new MLContext(seed: 0);
+            _trainingDataView = _mlContext.Data.LoadFromTextFile<SentimentData>(_trainDataPath, hasHeader: false);
+            var pipeline = ProcessData();
+            var trainingPipeline = BuildAndTrainModel(_trainingDataView, pipeline);
+            Evaluate(_trainingDataView.Schema);
+            PredictIssue();
+        }
+
+        public static IEstimator<ITransformer> ProcessData()
+        {
+            var pipeline = _mlContext.Transforms.Conversion.MapValueToKey(inputColumnName: "Sentiment", outputColumnName: "Label")
+                .Append(_mlContext.Transforms.Text.FeaturizeText(inputColumnName: "SentimentText", outputColumnName: "SentimentTextFeaturized"))
+.Append(_mlContext.Transforms.Concatenate("Features", "SentimentTextFeaturized"))
+.AppendCacheCheckpoint(_mlContext);
+
+            return pipeline;
+        }
+
+        public static IEstimator<ITransformer> BuildAndTrainModel(IDataView trainingDataView, IEstimator<ITransformer> pipeline)
+        {
+            var trainingPipeline = pipeline.Append(_mlContext.MulticlassClassification.Trainers.SdcaMaximumEntropy("Label", "Features"))
+        .Append(_mlContext.Transforms.Conversion.MapKeyToValue("PredictedLabel"));
+            _trainedModel = trainingPipeline.Fit(trainingDataView);
+            _predEngine = _mlContext.Model.CreatePredictionEngine<SentimentData, SentimentPrediction>(_trainedModel);
+            SentimentData issue = new SentimentData()
+            {
+                 SentimentText = "My rating is 4"
+            };
+            var prediction = _predEngine.Predict(issue);
+            Console.WriteLine($"=============== Single Prediction just-trained-model - Result: {prediction.Prediction} ===============");
+            return trainingPipeline;
+        }
+
+        public static void Evaluate(DataViewSchema trainingDataViewSchema)
+        {
+            var testDataView = _mlContext.Data.LoadFromTextFile<SentimentData>(_testDataPath, hasHeader: false);
+            var testMetrics = _mlContext.MulticlassClassification.Evaluate(_trainedModel.Transform(testDataView));
+            Console.WriteLine($"*************************************************************************************************************");
+            Console.WriteLine($"*       Metrics for Multi-class Classification model - Test Data     ");
+            Console.WriteLine($"*------------------------------------------------------------------------------------------------------------");
+            Console.WriteLine($"*       MicroAccuracy:    {testMetrics.MicroAccuracy:0.###}");
+            Console.WriteLine($"*       MacroAccuracy:    {testMetrics.MacroAccuracy:0.###}");
+            Console.WriteLine($"*       LogLoss:          {testMetrics.LogLoss:#.###}");
+            Console.WriteLine($"*       LogLossReduction: {testMetrics.LogLossReduction:#.###}");
+            Console.WriteLine($"*************************************************************************************************************");
+            SaveModelAsFile(_mlContext, trainingDataViewSchema, _trainedModel);
+        }
+
+        private static void SaveModelAsFile(MLContext mlContext, DataViewSchema trainingDataViewSchema, ITransformer model)
+        {
+            mlContext.Model.Save(model, trainingDataViewSchema, _modelPath);
+        }
+
+        private static void PredictIssue()
+        {
+            ITransformer loadedModel = _mlContext.Model.Load(_modelPath, out var modelInputSchema);
+            SentimentData singleIssue = new SentimentData() { SentimentText = "My rating is 4" };
+            _predEngine = _mlContext.Model.CreatePredictionEngine<SentimentData, SentimentPrediction>(loadedModel);
+            var prediction = _predEngine.Predict(singleIssue);
+            Console.WriteLine($"=============== Single Prediction - Result: {prediction.Prediction} ===============");
+            Console.WriteLine($"=============== Single Prediction - Probability: {prediction.Probability} ===============");
+        }
+    }
+}
---
+++ MulticlassSample/MulticlassSample/SentimentData.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.ML.Data;
+
+namespace MulticlassSample
+{
+    public class SentimentData
+    {
+        [LoadColumn(0), ColumnName("SentimentText")]
+        public string SentimentText;
+
+        [LoadColumn(1), ColumnName("Sentiment")]
+        public string Sentiment;
+    }
+
+    public class SentimentPrediction
+    {
+
+        [ColumnName("PredictedLabel")]
+        public string Prediction { get; set; }
+
+        public float Probability { get; set; }
+    }
+}
